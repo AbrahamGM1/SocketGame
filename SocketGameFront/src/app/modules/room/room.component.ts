1570934eda455b8d0f34aaaa69a8fccefbdbc314
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-03-25 18:12:43 -0700

    Diseño del menú hecho

--- SocketGameFront/src/app/modules/room/room.component.ts
+++ SocketGameFront/src/app/modules/room/room.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss'
 })
 export class RoomComponent {
+
+  games:string[] = ['Click Game', 'Tic Tac Toe', 'Type Game']
+  currentDescription:string = ''
+
+  changeDescription(game:string){
+    this.currentDescription = game;
+  }
 
 }
