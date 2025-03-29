2b95cd4be67ec8feee178aa82ab0e861bab1a161
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-03-28 23:40:26 -0700

    al ingresar el id de room se manda al usuario a dicha sala

--- SocketGameFront/src/app/modules/room/room.component.ts
+++ SocketGameFront/src/app/modules/room/room.component.ts
@@ -1,20 +1,66 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss'
 })
 export class RoomComponent {
 
   games:string[] = ['Click Game', 'Tic Tac Toe', 'Type Game']
-  currentDescription:string = ''
+  gameSelected:string = ''
+
+  formJoin:FormGroup;
+  formCreate:FormGroup;
+
+  optionSelected:boolean = false;
+  isCreate:boolean = false;
+  isJoin:boolean = false;
+
+  constructor(private fb:FormBuilder, private router:Router){
+    this.formJoin = fb.group({
+      idroom: ['',[Validators.required]]
+    })
+
+    this.formCreate = fb.group({
+      idroom: ['',[Validators.required]]
+    })
+  }
 
   changeDescription(game:string){
-    this.currentDescription = game;
+    this.gameSelected = game;
+  }
+
+  sendFormJoin(){
+    if (this.formJoin.valid) {
+      this.router.navigate(['/clickgame/'+this.formJoin.value.idroom])
+    } 
+  }
+
+  sendFormCreate(){
+    if(this.formCreate.valid){
+      this.router.navigate(['/clickgame/'+this.formCreate.value.idroom])
+    }
+  }
+
+  selectOption(isCreate:boolean){
+
+    if(this.optionSelected === false) {
+      this.optionSelected = true;
+    }
+
+    if(isCreate){
+      this.isCreate = true;
+      this.isJoin = false;
+    } else {
+      this.isCreate = false;
+      this.isJoin = true;
+    }
   }
 
 }
